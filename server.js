952178e9f9bce3e50044b6b11f524af0deb762e1
Jonathan Alejandro Guzman Rodriguez <jonathan.guzmanr@softtekswa.com>
2019-09-18 14:55:50 -0500

    feat: add middleware for metrics

--- server.js
+++ server.js
@@ -1,59 +1,37 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
- 
-// Use the prom-client module to expose our metrics to Prometheus
-const client = require('prom-client');
- 
-// enable prom-client to expose default application metrics
-const collectDefaultMetrics = client.collectDefaultMetrics;
- 
-// define a custom prefix string for application metrics
-collectDefaultMetrics({ prefix: 'nodejs_app:' });
- 
-// a custom histogram metric which represents the latency
-// of each call to our API /api/greeting.
-const histogram = new client.Histogram({
-  name: 'nodejs_app:api_gretting',
-  help: 'Duration of HTTP requests in ms',
-  labelNames: ['method', 'status_code'],
-  buckets: [0.1, 5, 15, 50, 100, 500]
-});
- 
-// create the express application
+const promMid = require('express-prometheus-middleware');
 const app = express();
 const port = process.argv[2] || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-const PrometheusMetrics = {
-  requestCounter: new client.Counter('counter_request', 'The number of requests served')
-}
 
-app.use((req, res, next) => {
-PrometheusMetrics.requestCounter.inc()
-next()
-})
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+}));
 
-// our API
 app.use('/api/greeting', (request, response) => {
-  // start the timer for our custom metric - this returns a function
-  // called later to stop the timer
-  const end = histogram.startTimer();
   const name = request.query.name ? request.query.name : 'World';
   response.send({content: `Hello, ${name}!`});
-  // stop the timer
-  end({ method: request.method, 'status_code': 200 });
 });
  
-// expose our metrics at the default URL for Prometheus
-app.get('/metrics', (request, response) => {
-  response.set('Content-Type', client.register.contentType);
-  response.send(client.register.metrics());
+
+app.use('/resolve/google', (request, response) => {
+  require('dns').resolve('www.google.com', function(err) {
+  if (err) {
+    response.send({content: "No Connection"});
+  } else {
+    response.send({content: "Connected"});
+  }
+  })
 });
 
-app.get('/metrics/counter', (req, res) => {
-	res.set('Content-Type', client.register.contentType);
-	res.end(client.register.getSingleMetricAsString('test_counter'));
-}); 
+app.use('/error', (request, response) => {
+  response.status(500).send();
+});
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
